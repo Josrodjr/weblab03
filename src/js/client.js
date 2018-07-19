603e86bb5c148cb3ecb31424ff0f010d9c2ce0c8
Unknown <Josrodjr@gmail.com>
2018-07-18 19:16:19 -0600

    three buttons
    buttons for message state

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,28 @@
+/* eslint-env node, browser */
+
 const state = {
-  //estado de carga
-  //mensajes
-  //filtro activo
+  //  estado de carga
+  //  mensajes
+  //  filtro activo
 };
 
-const render = newState => {
-}
+const render = (newState) => {
+  const options = document.createElement('div');
+  options.className = 'options';
 
-render (state);
+  for (let i = 0; i < 3; i += 1) {
+    const buttonname = document.createElement('button');
+    buttonname.id = `button ${i}`;
+    options.append(buttonname);
+  }
+
+
+  //  main render
+  const root = document.getElementById('root');
+  root.append(options);
+
+  // render(newState);
+};
+//  no poner el fetch dentro del render
+
+render(state);
